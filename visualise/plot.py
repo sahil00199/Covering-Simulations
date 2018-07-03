009e9e2f7eec9a149145891346e2332d0f98e8e0
Sahil Shah <sahilshah00199@gmail.com>
2018-07-03 16:10:43 +0200

    started with visualisation

---
+++ visualise/plot.py
@@ -0,0 +1,25 @@
+import matplotlib
+import matplotlib.pyplot as plt 
+
+file = open("input", 'r')
+data = ""
+for chunk in file:
+	data += chunk
+file.close()
+data = data.split("\n")
+data = [x for x in data if x != ""]
+
+ax = plt.subplot(aspect = 'equal')
+for element in data:
+	radius = float(element.split()[0])
+	coods = element.split()[1][1:-1].split(",")
+	x = float(coods[0])
+	y = float(coods[1])
+	circle = plt.Circle((x, y), radius, fill = False)
+	ax.add_artist(circle)
+
+
+plt.xlim((-1, 2))
+plt.ylim((-1, 2))
+
+plt.show()
